47eef3d0448f026709b5a0533bf97307608a6fe0
Andreas Nedbal <andy@pixelde.su>
2020-07-19 12:39:40 +0200

    📝 add documentation

--- src/index.js
+++ src/index.js
@@ -6,10 +6,30 @@
  * https://github.com/pixeldesu/moduleRaid/blob/master/LICENSE
  */
 
+ /**
+  * Main moduleRaid function
+  * 
+  * @param {boolean} debug - boolean value if debug output should be enabled
+  */
 export function moduleRaid (debug) {
+  /**
+   * A random generated module ID we use for injecting into Webpack
+   */
   moduleRaid.mID  = Math.random().toString(36).substring(7);
+
+  /**
+   * Storage for the modules we extracted from Webpack
+   */
   moduleRaid.mObj = {};
+
+  /**
+   * Storage for the constructors we extracted from Webpack
+   */
   moduleRaid.cArr = [];
+
+  /**
+   * Intermediary storage for __webpack_require__ if we were able to extract it
+   */
   moduleRaid.mGet = null;
 
   if (debug) {
@@ -20,12 +40,22 @@ export function moduleRaid (debug) {
     moduleRaid.debug = false;
   }
 
+  /**
+   * Debug logging method, works when true is passed as an argument to the main
+   * moduleRaid function, or when window.mRdebug is set to true
+   * 
+   * @param {*} message - The message to be logged
+   */
   moduleRaid.log = (message) => {
     if (moduleRaid.debug) {
       console.warn(`[moduleRaid] ${message}`);
     }
   };
 
+  /**
+   * An array containing different argument injection methods for
+   * Webpack, and subsequently pulling out methods and modules
+   */
   moduleRaid.args = [
     [[0], [(e, t, i) => {
       mCac = i.c;
@@ -45,6 +75,10 @@ export function moduleRaid (debug) {
     }}, [[moduleRaid.mID]]]
   ]
 
+  /**
+   * Method that will try to inject a module into Webpack or get modules
+   * depending on it's success it might be more or less brute about it
+   */
   const fillModuleArray = () => {
     if (typeof webpackJsonp === 'function') {
       moduleRaid.args.forEach((argument, index) => {
@@ -87,10 +121,27 @@ export function moduleRaid (debug) {
 
   fillModuleArray();
 
+  /**
+   * Custom getter for stored Webpack modules
+   * 
+   * NOTE: If it can be extracted, mR.get will be the default __webpack_require__
+   * method, this is only a simple fallback
+   * 
+   * @param {*} id - ID of the module to get
+   */
   const get = (id) => {
     return moduleRaid.mObj[id]
   };
 
+  /**
+   * Method to search through the module object, searching for the fitting key
+   * if a string is supplied
+   * 
+   * If query is supplied as a function, everything that returns true when passed
+   * to the query function will be returned
+   * 
+   * @param {string|Function} query 
+   */
   const findModule = (query) => {
     results = [];
     modules = Object.keys(moduleRaid.mObj);
@@ -123,6 +174,15 @@ export function moduleRaid (debug) {
     return results;
   };
 
+  /**
+   * Method to search through the constructor array, searching for the fitting key
+   * if a string is supplied
+   * 
+   * If query is supplied as a function, everything that returns true when passed
+   * to the query function will be returned
+   * 
+   * @param {string|Function} query 
+   */
   const findFunction = (query) => {
     if (moduleRaid.cArr.length == 0) {
       throw Error('No module constructors to search through!');
